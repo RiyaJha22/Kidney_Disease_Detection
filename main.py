26f088fdb151603df7a9d3a8e479bc63ad54c787
Riya Jha <riya.pict@gmail.com>
2024-08-04 12:56:21 +0530

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
+import sys
+import os
+
+# Print the current Python path for debugging
+print(sys.path)
+
+# Add the cnnClassifier directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'cnnClassifier')))
+
+# Import the logger from cnnClassifier
+from cnnClassifier import logger
+
+# Your main code here
+print("Import successful!")
+
 
 
 
@@ -21,6 +38,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
